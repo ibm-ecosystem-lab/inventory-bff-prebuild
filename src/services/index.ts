cb8058879c932ae5942219e544e25ccc116ecfec
Noé Samaille <noe.samaille@ibm.com>
2022-07-27 16:37:24 +0200

    Updates
    Signed-off-by: Noé Samaille <noe.samaille@ibm.com>

--- src/services/index.ts
+++ src/services/index.ts
@@ -3,6 +3,9 @@ import { Container } from "typescript-ioc";
 export * from './hello-world.api';
 export * from './project.api';
 export * from './task.api';
+export * from './stock-items.api';
+export * from './stock-items-mock.service';
+export * from './stock-items.service';
 
 import config from './ioc.config';
 
